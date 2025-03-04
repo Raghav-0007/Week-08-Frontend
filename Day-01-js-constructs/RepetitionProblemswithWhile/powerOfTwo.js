40e814b0fc92eb1d40cd6722704e221116cd7ac2
Raghav-0007 <rmyadav514@gmail.com>
2025-03-04 12:24:33 +0530

    [Radhamohan yadav] . add completed powerOfTwo.js

---
+++ Day-01-js-constructs/RepetitionProblemswithWhile/powerOfTwo.js
@@ -0,0 +1,22 @@
+// Get the number 'n' from command-line arguments
+const args = process.argv.slice(2);
+
+if (args.length < 1) {
+    console.log("Please provide a number.");
+    process.exit(1);
+}
+
+let n = parseInt(args[0], 10);
+
+if (isNaN(n) || n < 0) {
+    console.log("Invalid input! Please enter a non-negative integer.");
+    process.exit(1);
+}
+
+// Print the table of powers of 2
+console.log(`Table of powers of 2 up to 2^${n} (stopping at 256):`);
+for (let i = 0; i <= n; i++) {
+    let power = Math.pow(2, i);
+    if (power > 256) break;
+    console.log(`2^${i} = ${power}`);
+}
